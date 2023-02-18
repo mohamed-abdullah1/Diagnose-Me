bf5293eecabb57bb44424288a9ed19286d3de05e
0x41ly <alykhaledxd@gmail.com>
2023-02-18 22:59:22 +0200

    hotFix

--- BackEnd/DiagnoseMe.MicroServices/Auth/Auth.Application/Authentication/Commands/AddUserToRole/AddUserToRoleCommandHandler.cs
+++ BackEnd/DiagnoseMe.MicroServices/Auth/Auth.Application/Authentication/Commands/AddUserToRole/AddUserToRoleCommandHandler.cs
@@ -11,18 +11,19 @@ public class AddUserToRoleCommandHandler :
     public async Task<ErrorOr<AuthenticationResult>> Handle(AddUserToRoleCommand command, CancellationToken cancellationToken)
     {
         var results = new AuthenticationResult();
-        if (!Roles.AvailableRoles.Contains(command.Role))
+        var role = command.Role[0].ToString().ToUpper() + command.Role.Substring(1).ToLower();
+        if (!Roles.AvailableRoles.Contains(role))
             return Errors.Role.RoleNotExist;
 
         var user = await _userManager.FindByNameAsync(command.UserName);
         if (user == null)
             return Errors.User.Name.NotExists;
 
-        var result = await _userManager.AddToRoleAsync(user, command.Role);
+        var result = await _userManager.AddToRoleAsync(user, role);
         if(!result.Succeeded)
             return Errors.User.MapIdentityError(result.Errors.ToList());
 
-        results.Message = $"User is successfully added to {command.Role}role";
+        results.Message = $"User is successfully added to {command.Role} role";
         return results;
     }
 }
