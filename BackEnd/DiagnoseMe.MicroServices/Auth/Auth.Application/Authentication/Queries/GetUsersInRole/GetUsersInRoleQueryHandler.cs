43b61fc7439137e811bd18d5801c434866239f2c
0x41ly <alykhaledxd@gmail.com>
2023-02-18 23:01:55 +0200

    hotFix

--- BackEnd/DiagnoseMe.MicroServices/Auth/Auth.Application/Authentication/Queries/GetUsersInRole/GetUsersInRoleQueryHandler.cs
+++ BackEnd/DiagnoseMe.MicroServices/Auth/Auth.Application/Authentication/Queries/GetUsersInRole/GetUsersInRoleQueryHandler.cs
@@ -9,11 +9,12 @@ public class GetUsersInRoleQueryHandler :
     ): base(userManager){}
     public async Task<ErrorOr<List<ApplicationUser>>> Handle(GetUsersInRoleQuery query, CancellationToken cancellationToken)
     {
-        if (!Roles.AvailableRoles.Contains(query.Role))
+        var role = query.Role[0].ToString().ToUpper() + query.Role.Substring(1).ToLower();
+        if (!Roles.AvailableRoles.Contains(role))
             return Errors.Role.RoleNotExist;
         
         var users = (await _userManager.
-                    GetUsersInRoleAsync(query.Role)).
+                    GetUsersInRoleAsync(role)).
                     OrderBy(u => u.UserName).
                     Skip((query.pageNumber -1)* 10).
                     Take(10).
