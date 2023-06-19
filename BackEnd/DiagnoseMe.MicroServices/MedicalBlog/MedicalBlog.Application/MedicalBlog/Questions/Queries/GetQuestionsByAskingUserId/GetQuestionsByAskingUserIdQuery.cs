21e2049c6c4f5cb53bee88b55277e61a2e3c3060
0x41ly <alykhaledxd@gmail.com>
2023-06-19 10:28:01 +0300

    medicalblog hotfix

--- BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Application/MedicalBlog/Questions/Queries/GetQuestionsByAskingUserId/GetQuestionsByAskingUserIdQuery.cs
+++ BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Application/MedicalBlog/Questions/Queries/GetQuestionsByAskingUserId/GetQuestionsByAskingUserIdQuery.cs
@@ -1,9 +1,9 @@
 using ErrorOr;
 using MediatR;
-using MedicalBlog.Application.MedicalBlog.Questions.Common;
+using MedicalBlog.Application.MedicalBlog.Common;
 
 namespace MedicalBlog.Application.MedicalBlog.Questions.Queries.GetQuestionsByAskingUserId;
 
 public record GetQuestionsByAskingUserIdQuery(
     int PageNumber,
-    string AskingUserId) : IRequest<ErrorOr<List<QuestionResponse>>>;
+    string AskingUserId) : IRequest<ErrorOr<PageResponse>>;
--- BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Application/MedicalBlog/Questions/Queries/GetQuestionsByAskingUserId/GetQuestionsByAskingUserIdQueryHandler.cs
+++ BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Application/MedicalBlog/Questions/Queries/GetQuestionsByAskingUserId/GetQuestionsByAskingUserIdQueryHandler.cs
@@ -2,13 +2,14 @@ using ErrorOr;
 using MapsterMapper;
 using MediatR;
 using MedicalBlog.Application.Common.Interfaces.Persistence.IRepositories;
+using MedicalBlog.Application.MedicalBlog.Common;
 using MedicalBlog.Application.MedicalBlog.Questions.Common;
 using MedicalBlog.Domain.Common.Errors;
 
 namespace MedicalBlog.Application.MedicalBlog.Questions.Queries.GetQuestionsByAskingUserId;
 
 
-public class GetQuestionsByAskingUserIdQueryHandler :  IRequestHandler<GetQuestionsByAskingUserIdQuery, ErrorOr<List<QuestionResponse>>>
+public class GetQuestionsByAskingUserIdQueryHandler :  IRequestHandler<GetQuestionsByAskingUserIdQuery, ErrorOr<PageResponse>>
 {
     private readonly IQuestionRepository _questionRepository;
     private readonly IMapper _mapper;
@@ -24,7 +25,7 @@ public class GetQuestionsByAskingUserIdQueryHandler :  IRequestHandler<GetQuesti
         _userRepository = userRepository;
     }
 
-    public async Task<ErrorOr<List<QuestionResponse>>> Handle(GetQuestionsByAskingUserIdQuery query, CancellationToken cancellationToken)
+    public async Task<ErrorOr<PageResponse>> Handle(GetQuestionsByAskingUserIdQuery query, CancellationToken cancellationToken)
     {
         var user = await _userRepository.GetByIdAsync(query.AskingUserId);
         if (user == null)
@@ -34,10 +35,18 @@ public class GetQuestionsByAskingUserIdQueryHandler :  IRequestHandler<GetQuesti
             predicate: q => q.AskingUserId == query.AskingUserId,
             include: "Answers,AskingUser,Tags,AgreeingUsers"))
             .OrderByDescending(x => x.CreatedOn)
+            .ToList();
+        var IsNextPage = questions.Count > query.PageNumber * 10;
+        var resultQuestions = questions
             .Skip((query.PageNumber - 1) * 10)
+            .Take(10)
             .ToList();
 
         var questionsResponse = _mapper.Map<List<QuestionResponse>>(resultQuestions);
-        return questionsResponse;
+        return new PageResponse(
+            questionsResponse.Select(q => (object)q).ToList(),
+            query.PageNumber,
+            IsNextPage
+        );
     }
 }
