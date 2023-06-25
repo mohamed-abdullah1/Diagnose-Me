66b62a8146d7431ed363bb860785e4c4a32a9b80
0x41ly <alykhaledxd@gmail.com>
2023-06-25 16:01:35 +0300

    fixing an error

--- BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Infrastructure/RabbitMQ/MessageQueueSubscriber.cs
+++ BackEnd/DiagnoseMe.MicroServices/MedicalBlog/MedicalBlog.Infrastructure/RabbitMQ/MessageQueueSubscriber.cs
@@ -1,6 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using RabbitMQ.Client;
 
 namespace MedicalBlog.Infrastructure.RabbitMQ;
 
