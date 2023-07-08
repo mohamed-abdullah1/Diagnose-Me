407f57d2cd60c33b9ab9739c18ad957a69234a77
Mahmoud-Hamza-Git <mahmoudhamza9440@gmail.com>
2023-07-08 18:51:16 +0300

    changing patch to post requests

--- BackEnd/DiagnoseMe.MicroServices/node_backend/routes/appointmentRoutes.js
+++ BackEnd/DiagnoseMe.MicroServices/node_backend/routes/appointmentRoutes.js
@@ -12,19 +12,19 @@ const {
   getAvailableTimes,
 } = require('../controllers/appointmentControllers');
 
-router.route('/add-available-time/').patch(protect, addAvailableTime);
-router.route('/delete-available-time/').patch(protect, deleteAvailableTime);
+router.route('/add-available-time/').post(protect, addAvailableTime);
+router.route('/delete-available-time/').post(protect, deleteAvailableTime);
 router.route('/clear-available-times/').delete(protect, clearAvailableTimes);
 router.route('/get-available-times/:doctorId').get(protect, getAvailableTimes);
 
 router.route('/book-appointment').post(protect, bookAppointment);
 router.route('/delete-booked-appointment/:appointmentId').delete(protect, deleteBookedAppointment);
-router.route('/change-booked-status/:appointmentId').patch(protect, changeBookedStatus);
+router.route('/change-booked-status/:appointmentId').post(protect, changeBookedStatus);
 router.route('/get-all-booked-appointments/').get(protect, getAllBookedAppointments);
 
 module.exports = router;
 
-// router.route('/add-available-repeated-date/').patch(protect, addAvailableRepeatedDate);
-// router.route('/update-available-repeated-date/:dateId').patch(protect, updateRepeatedDate);
-// router.route('/clear-repeated-dates/').patch(protect, clearRepeatedDates);
-// router.route('/update-available-individual-date/:dateId').patch(protect, updateIndiviualDate);
+// router.route('/add-available-repeated-date/').post(protect, addAvailableRepeatedDate);
+// router.route('/update-available-repeated-date/:dateId').post(protect, updateRepeatedDate);
+// router.route('/clear-repeated-dates/').post(protect, clearRepeatedDates);
+// router.route('/update-available-individual-date/:dateId').post(protect, updateIndiviualDate);
