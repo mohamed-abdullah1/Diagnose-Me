837b5670d4098bab56a7ed4e635f3520514e8881
0x41ly <alykhaledxd@gmail.com>
2023-07-13 14:36:07 +0300

    hot fix

--- BackEnd/DiagnoseMe.MicroServices/MedicalServices/MedicalServices.Application/MedicalServices/Patients/Common/PatientResponse.cs
+++ BackEnd/DiagnoseMe.MicroServices/MedicalServices/MedicalServices.Application/MedicalServices/Patients/Common/PatientResponse.cs
@@ -1,14 +1,13 @@
+using MedicalServices.Application.MedicalServices.Common;
+
 namespace MedicalServices.Application.MedicalServices.Patients.Common;
 
 public record PatientResponse(
-    string Id,
-    string Name,
-    string FullName,
-    string ProfilePictureUrl, 
+    UserResponse User,
     string Hight,
     string Weight,
     List<string>? Allergies,
     List<string>? Diseases,
     List<string>? Medications,
-    List<string>? Surgeries,
-    List<CheckResponse>? Checks);
+    List<string>? Surgeries
+    );
