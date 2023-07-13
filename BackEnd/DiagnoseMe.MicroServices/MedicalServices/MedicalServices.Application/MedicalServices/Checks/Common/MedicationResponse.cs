8d00e4d92be8ff2cbc3412b66bdf1ff2e9e64227
0x41ly <alykhaledxd@gmail.com>
2023-07-13 04:51:01 +0300

    hot fix

--- BackEnd/DiagnoseMe.MicroServices/MedicalServices/MedicalServices.Application/MedicalServices/Checks/Common/MedicationResponse.cs
+++ BackEnd/DiagnoseMe.MicroServices/MedicalServices/MedicalServices.Application/MedicalServices/Checks/Common/MedicationResponse.cs
@@ -1,4 +1,9 @@
 namespace MedicalServices.Application.MedicalServices.Checks.Common;
 
 public record MedicationResponse(
+    string Id,
+    string Name,
+    string Dosage,
+    string Frequency,
+    string Notes
 );
