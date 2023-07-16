8de5871016323acf722a8449337fdaf000af3022
Mahmoud-Hamza-Git <mahmoudhamza9440@gmail.com>
2023-07-16 14:00:40 +0300

    adding stripe & notification

--- BackEnd/DiagnoseMe.MicroServices/node_backend/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const { protect } = require('../middleware/authMiddleware');
-const { checkout } = require('../controllers/bookingControllers');
-const router = require('express').Router();
-
-//
-
-router.route('/payment').post(protect, checkout);
-
-module.exports = router;
